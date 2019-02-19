186dc98634e1f9f9259e2fa0e0e8e574f41353b1
Yash Khare <yashsja@gmail.com>
2019-02-19 20:14:50 +0530

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -3,7 +3,7 @@ void main() {
   temp = temp/5;
   print(temp);
   var temp2 = 25;
-  //temp2 = temp2/5; //will give an error as temp is stored as 	 integer by default
+  temp2 = temp2/5; //will give an error as temp is stored as 	 integer by default
   int y = 4;
   y+=4;
   print(y);

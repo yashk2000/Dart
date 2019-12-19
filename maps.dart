0ca4400d36a11910c69a8f63f8557872b7d20f6d
Shashank Priyadarshi <52889867+robustTechie@users.noreply.github.com>
2019-12-19 18:22:48 +0530

    Update maps.dart

--- maps.dart
+++ maps.dart
@@ -3,7 +3,7 @@ void main() {
     1: 'one',
     2: 'two'
   };
-  print(map[2]);//two
+  print(map[2]);
 
   var demoMap = {
     '1': 1,
@@ -14,18 +14,18 @@ void main() {
   print(demoMap.length);
 
   map[4] = 'four';
-  print(map[4]);//four
+  print(map[4]);
 
   var listMap = {
     1: [1,2,3],
     2: [4,5,6]
   };
-  print(listMap[1]);//[1, 2, 3]
-  print(listMap[1][2]);//3
+  print(listMap[1]);
+  print(listMap[1][2]);
 
 var gifts = Map();
 gifts['first'] = 'partridge';
 gifts['second'] = 'turtledoves';
 gifts['fifth'] = 'golden rings';
-print(gifts);//{first: partridge, second: turtledoves, fifth: golden rings}
+print(gifts);
 }
